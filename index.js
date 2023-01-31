a4f53500232670eab16c5db08a8e9dbd1e3d67f5
Emad Saeed <eaf.saeed@gmail.com>
2023-01-31 14:36:28 +0000

    Emplpyee class

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const questions = require('./utils/questions')
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,7 +10,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
