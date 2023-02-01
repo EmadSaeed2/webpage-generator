5994aa1e77348d3315033b090f7c72f37d3a162e
Emad Saeed <eaf.saeed@gmail.com>
2023-02-01 13:51:26 +0000

    README update

--- index.js
+++ index.js
@@ -21,24 +21,23 @@ function writeToFile(filePath, data) {
 function showOptions() {
     inquirer.prompt(questions.options)
         .then((selection) => {
-            console.log(selection)
+            // console.log(selection)
             if (selection.add_more === `Add an engineer`) {
                 inquirer.prompt(questions.engineer_quetions).then((engineer_data) => {
-                    console.log(engineer_data)
+                    // console.log(engineer_data)
                     const new_engineer = new Engineer(engineer_data.engineer_name, engineer_data.engineer_id, engineer_data.engineer_email, engineer_data.engineer_github)
                     team.push(new_engineer)
                     showOptions()
                 })
             } else if (selection.add_more === `Add an intern`) {
                 inquirer.prompt(questions.intern_questions).then((intern_data) => {
-                    console.log(intern_data)
+                    // console.log(intern_data)
                     const new_intern = new Intern(intern_data.intern_name, intern_data.intern_id, intern_data.intern_email, intern_data.intern_school)
                     team.push(new_intern)
                     showOptions()
                 })
             } else {
                 const html = render(team)
-                console.log(html)
                 writeToFile('./index.html', html)
             }
         })
@@ -49,7 +48,7 @@ function init() {
     inquirer
         .prompt(questions.manager_questions)
         .then((manager_data) => {
-            console.log(manager_data)
+            // console.log(manager_data)
             const new_manager = new Manager(manager_data.manager_name, manager_data.manager_id, manager_data.manager_email, manager_data.manager_office_no)
             showOptions()
             team.push(new_manager)
